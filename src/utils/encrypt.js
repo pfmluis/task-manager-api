4e4316978d81933bf525067bdba5629e67e94841
Pedro Luís <pfmluis@gmail.com>
2021-12-12 22:13:34 +0100

    feat: create encryption layer

---
+++ src/utils/encrypt.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt'
+
+function encrypt(string) {
+  const saltRounds = process.env.SALT_ROUNDS
+
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) reject(err)
+
+      bcrypt.hash(string, salt, (err, hash) => {
+        if (err) reject(err)
+
+        resolve(hash)
+      })
+    })
+  })
+}
+
+function compare(decryptedString, encryptedString) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(decryptedString, encryptedString, (err, isSame) => {
+      if (err) reject(err)
+
+      resolve(isSame)
+    })
+  })
+}
+
+export default Object({
+  encrypt,
+  compare
+})
