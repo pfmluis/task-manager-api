b313fdac004c7912697b1412c06590d05bcbdbef
Pedro Luís <pfmluis@gmail.com>
2021-12-12 22:14:01 +0100

    feat: create password validator

---
+++ src/domain/entities/validators/password-validator.js
@@ -0,0 +1,16 @@
+import Joi from 'joi';
+
+export default function passwordValidator(password) {
+  const schema = Joi.string()
+    .min(8)
+    .max(50)
+    .pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]*/)
+    .error(() => 'Password must contain a between 8 and 50 characters, and at least one number and one symbol')
+
+  const { error } = schema.validate(password)
+
+  return {
+    isValid: !error,
+    error
+  }
+}
