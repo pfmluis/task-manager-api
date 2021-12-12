c9c2d289a0ee77b17ff944f8e27b41852634b452
Pedro Luís <pfmluis@gmail.com>
2021-12-12 18:50:31 +0100

    feat: add makeUser factory

---
+++ src/domain/entities/user.js
@@ -0,0 +1,26 @@
+export default function buildMakeUser({ idManager, validator }) {
+  return ({
+    sid = idManager.generate(),
+    name,
+    email,
+    hash,
+    role,
+    permissions = []
+  }) => {
+
+    const { isValid, error } = validator({ sid, name, email, hash, role })
+
+    if (isValid) throw new Error(error)
+
+    return Object.freeze({
+      getSid: () => sid,
+      getName: () => name,
+      getUser: () => email,
+      role: () => role,
+      getPermissions: () => permissions,
+      setPassword: (hash) => {
+        hash = hash
+      } 
+    })
+  }
+}
