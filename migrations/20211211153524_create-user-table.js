7bff01bf75e20967c9881cd222133db2a26bb861
Pedro Luís <pfmluis@gmail.com>
2021-12-12 23:25:11 +0100

    chore: make email unique in db

--- migrations/20211211153524_create-user-table.js
+++ migrations/20211211153524_create-user-table.js
@@ -3,7 +3,7 @@ exports.up = async function(knex) {
   return knex.schema.createTable('user', function(table) {
     table.string('sid', 36).primary()
     table.string('name').notNullable()
-    table.string('email').notNullable()
+    table.string('email').notNullable().unique()
     table.string('hash').notNullable()
     table.string('role_name', 25).notNullable().references('name').inTable('role')
     table.timestamp('created_at').defaultTo(knex.fn.now())
