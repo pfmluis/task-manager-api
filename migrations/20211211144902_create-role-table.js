28b4bf0054d6d4bd7e23e8c5e1c0cff3c0b8213d
Pedro Luís <pfmluis@gmail.com>
2021-12-12 16:41:52 +0100

    refactor: remove semicolons from migration files

--- migrations/20211211144902_create-role-table.js
+++ migrations/20211211144902_create-role-table.js
@@ -1,14 +1,14 @@
 
 exports.up = async function(knex) {
   return knex.schema.createTable('role', function(table) {
-    table.increments('id').primary();
-    table.string('name').notNullable();
+    table.increments('id').primary()
+    table.string('name').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
     table.unique('name')
   })
-};
+}
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('role');
-};
+  return knex.schema.dropTable('role')
+}
