32caa7fa1a1ae5b0c9820029c21bf6c8f8822b88
Pedro Luís <pfmluis@gmail.com>
2021-12-13 14:11:46 +0100

    feat: add findByEmailWithRoleAndPermissions in userDb

--- src/data/user-db.js
+++ src/data/user-db.js
@@ -9,7 +9,15 @@ export default function makeUserDb({ connection }) {
   }
 
   function findByEmail(email) {
-    return connection('user').where('email', email)
+    return connection('user').where('email', email).first()
+  }
+
+  async function findByEmailWithRoleAndPermissions(email) {
+    const result = await connection('user')
+      .where('user.email', email)
+      .innerJoin('role_permission', 'user.role_name', 'role_permission.role_name')
+      .innerJoin('permission', 'permission.key', 'role_permission.permission_key')
+    return adaptFindUserWithPermissionsToUser(result)
   }
 
   function mapCreateUserData(user) {
@@ -23,8 +31,26 @@ export default function makeUserDb({ connection }) {
     }
   }
 
+  function adaptFindUserWithPermissionsToUser(data) {
+    if (!data || !Array.isArray(data)) return {}
+
+    return data.reduce((previous, current) => ({
+      name: current.name,
+      sid: current.sid,
+      email: current.email,
+      hash: current.hash,
+      role: current.role_name,
+      createdAt: current.created_at,
+      updatedAt: current.updated_at,
+      permissions: [ ...previous.permissions, current.permission_key ]
+    }), {
+      permissions: []
+    })
+  }
+
   return Object.freeze({
     createUser,
-    findByEmail
+    findByEmail,
+    findByEmailWithRoleAndPermissions
   })
 }
