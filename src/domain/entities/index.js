5ee441ba9f53dc5d64fedce0b082ffe5de040095
Pedro Luís <pfmluis@gmail.com>
2021-12-13 14:12:53 +0100

    feat: inject dependencies in userLogin entity

--- src/domain/entities/index.js
+++ src/domain/entities/index.js
@@ -1,11 +1,14 @@
 import makeId from './id'
 import userValidator from './validators/user-validator'
 import buildMakeUser from './user'
+import buildMakeUserLogin from './user-login'
+import userLoginValidator from './validators/user-login-validator'
 
 const idManager = makeId()
 
 const makeUser = buildMakeUser({ idManager, validator: userValidator })
+const makeUserLogin = buildMakeUserLogin({ validator: userLoginValidator })
 
-const entities = Object.freeze({ makeUser })
+const entities = Object.freeze({ makeUser, makeUserLogin })
 
 export default entities
